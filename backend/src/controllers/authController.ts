910aea9a76669cc9ee0d45608fec24e3338eec47
Lucas Gonçalves <ldgoncalvs@gmail.com>
2025-05-21 10:17:16 -0300

    add: employee registration

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,23 +1,54 @@
-import { Request, Response } from 'express';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import Employee from '../models/Employee';
+import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import Employee from "../models/Employee";
 
-const JWT_SECRET = process.env.JWT_SECRET || 'default_secret';
+const JWT_SECRET = process.env.JWT_SECRET || "default_secret";
+
+const generateToken = (id: string): string => {
+  const token = jwt.sign({ id }, JWT_SECRET, { expiresIn: "1h" });
+  return token;
+};
 
 export const login = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  const user = await Employee.findOne({ email });
-  if (!user){
-    res.status(401).json({ error: 'Invalid credentials' });
-    return;
+    const user = await Employee.findOne({ email });
+    if (!user) {
+      res.status(401).json({ error: "Invalid credentials" });
+      return;
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    const token = generateToken(user._id.toString());
+
+    res.json({ token });
+  } catch {
+    res.status(500).json({ message: "Error logging in" });
   }
-  const isMatch = await bcrypt.compare(password, user.password);
-  if (!isMatch){
-    res.status(401).json({ error: 'Invalid credentials' });
+};
 
-  } 
-  const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: '1h' });
-  res.json({ token });
+export const register = async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const existingEmployee = await Employee.findOne({ email });
+    if (existingEmployee) {
+      res
+        .status(400)
+        .json({
+          message: "Employee already registered, do you want them to login?",
+        });
+    }
+
+    const user = await Employee.create({ name, email, password });
+    const token = generateToken(user._id.toString());
+    res.status(201).json({ token });
+  } catch (err) {
+    res.status(500).json({ message: "Error registering employee" });
+  }
 };
